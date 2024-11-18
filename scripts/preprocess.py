9c598ae37142e8a1fe181409da67ee530f0ec36e
David TCHATCHOUA <davidtchouta@gmail.com>
2024-11-18 15:11:17 +0000

    debut EDA

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from utils import data_import, removed
+from utils import data_import, removed, determine_season
 
 # Appel de la fonction avec un chemin de fichier
 file_path = "../data/perf_kilo.xlsx"
@@ -17,5 +17,12 @@ if __name__ == "__main__":
     # Étape 2 : Nettoyer les données en supprimant les lignes vides
     cleaned_df = removed(data)
     cleaned_df.to_csv('../data/processed_data.csv', index=False)
-    cleaned_df.info()
+    #cleaned_df.info()
+    #ajouter la colonne season
+    # Appliquer la fonction determine_season à la colonne 'day'
+    cleaned_df['season'] = cleaned_df['day'].apply(determine_season)
+    # Afficher le DataFrame avec les saisons
+    print(cleaned_df)
+    
+
 
--- scripts/utils.py
+++ scripts/utils.py
@@ -22,5 +22,55 @@ def removed(df):
     print("\n\n")
     return cleaned_df
 
+# function for debugging purpose
 def bonjour():
     return "Hello world !"
+
+# adding a saison period variable
+
+
+
+def determine_season(day):
+    from datetime import datetime
+    # Définir les intervalles saisonniers
+    summer_intervals = [
+        (datetime(2021, 6, 18), datetime(2021, 9, 20)),
+        (datetime(2022, 6, 21), datetime(2022, 9, 20)),
+        (datetime(2023, 6, 21), datetime(2023, 9, 20)),
+        (datetime(2024, 6, 21), datetime(2024, 9, 20)),
+    ]
+
+    autumn_intervals = [
+        (datetime(2021, 9, 21), datetime(2021, 12, 20)),
+        (datetime(2022, 9, 21), datetime(2022, 12, 20)),
+        (datetime(2023, 9, 21), datetime(2023, 12, 20)),
+        (datetime(2024, 9, 21), datetime(2024, 11, 14)),  # Fin limitée à 14/11/2024
+    ]
+
+    winter_intervals = [
+        (datetime(2021, 12, 21), datetime(2022, 3, 20)),
+        (datetime(2022, 12, 21), datetime(2023, 3, 20)),
+        (datetime(2023, 12, 21), datetime(2024, 3, 20)),
+    ]
+
+    spring_intervals = [
+        (datetime(2022, 3, 21), datetime(2022, 6, 20)),
+        (datetime(2023, 3, 21), datetime(2023, 6, 20)),
+        (datetime(2024, 3, 21), datetime(2024, 6, 20)),
+    ]
+
+    # Vérifier dans quelle saison la date se trouve
+    for start, end in summer_intervals:
+        if start <= day <= end:
+            return "Été"
+    for start, end in autumn_intervals:
+        if start <= day <= end:
+            return "Automne"
+    for start, end in winter_intervals:
+        if start <= day <= end:
+            return "Hiver"
+    for start, end in spring_intervals:
+        if start <= day <= end:
+            return "Printemps"
+
+    return "Date hors période spécifiée"
